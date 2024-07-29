a05df0790708a3d6b153693006e016eebe8ea501
Franklin Munoz <v-frmunoz@microsoft.com>
2024-07-29 15:42:19 -0700

    :((

--- mind-bridge/src/App.js
+++ mind-bridge/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import InitialSignInPage from "./components/InitialSignInPage";
 
-function App() {
+async function App() {
   return (
     <div className="App-header">
       <InitialSignInPage />
