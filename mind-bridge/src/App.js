f739528fddef22eed298d81c49121d8e1bfe7b67
AyoOgunleye <v-ayogunleye@microsoft.com>
2024-07-31 15:51:34 -0700

    finished

--- mind-bridge/src/App.js
+++ mind-bridge/src/App.js
@@ -3,13 +3,15 @@ import InitialSignInPage from './components/InitialSignInPage'
 import ProfSignIn from './components/ProfSignIn'
 import ProfSignUp from './components/ProfSignUp'
 import EditProfilePage from './components/EditProfilePage'
+import PatientEditProfilePage from './components/PatientEditProfilePage'
 import ProfOverview from './components/ProfOverview'
 import PatientSpotify from './components/PatientSpotify'
 import PatientLandingPage from './components/PatientLandingPage'
 import './App.css'
 import { useEffect, useState } from 'react'
 import ListPage from './components/ListPage'
-import PatientSignIn from './components/PatientSignIn'
+import PatientSIgnIn from './components/PatientSIgnIn'
+import PatientSignUp from './components/PatientSignUp'
 
 
 function App() {
@@ -29,8 +31,12 @@ function App() {
           <Route path="/PatientSpotify" element={<PatientSpotify  />} />
           <Route path="/profOverview" element={<ProfOverview  />} />
           <Route path="/listPage" element={<ListPage  />} />
-          <Route path="/patientSignIn" element={<PatientSignIn  />} />
+          <Route path="/patientSignIn" element={<PatientSIgnIn  setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path= "/PatientSignUp" element={ <PatientSignUp setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
           <Route path="/patientLandingPage" element={<PatientLandingPage  />} />
+          <Route path="/PatientEditProfilePage" element={<PatientEditProfilePage  />} />
+
+
 
         </Routes>
       </BrowserRouter>
