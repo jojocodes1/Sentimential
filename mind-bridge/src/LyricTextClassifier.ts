5b669a5b8c3547d63b4b8fa582894276d64da848
Franklin Munoz <v-frmunoz@microsoft.com>
2024-08-01 10:04:43 -0700

    fixes to the LyricTextClassifier class and progress on clientListeningClassification class

--- mind-bridge/src/LyricTextClassifier.ts
+++ mind-bridge/src/LyricTextClassifier.ts
@@ -3,13 +3,18 @@
  * Date of Last Update: 7/30/2024
  * Description: Class for calling into the rest api for the custom text classification model, contains one method which takes in a string (lyrics) and returns its classification as a string.
  */
+
+const delay = ms => new Promise( res => setTimeout(res, ms));
+
 export class LyricTextClassifier{
 
   //singleton pattern  
   public static I = new LyricTextClassifier();
   private constructor() { };
 
-  async Classify(lyric: String): Promise<String> {
+  
+
+  async Classify(lyric: String ): Promise<String> {
 
     const bodytext = {
       displayName: "Classifying documents",
@@ -45,22 +50,21 @@ export class LyricTextClassifier{
         headers: { 'Ocp-Apim-Subscription-Key': '0344a2e729204d3b9c7091d9a2fe25fb'}  
     };
   
-    const response = await fetch('https://psionicsynctc.cognitiveservices.azure.com/language/analyze-text/jobs?api-version=2022-05-01', reqOptions);
+    return new Promise((resolve) => {
+      fetch('https://psionicsynctc.cognitiveservices.azure.com/language/analyze-text/jobs?api-version=2022-05-01', reqOptions).then((response)=> {
 
-    if (response.status === 202) {
-      //extract url from operation location header field
-    const results = await fetch(response.headers['operation-location'], reqResult);
-    console.log(response.headers);
+        delay(3000).then(()=>{
+          const operationLocation = response.headers.get('operation-location')!;
 
-
-    //TODO parse results and 
-    const data = await results.json();
-    //const dataParse = JSON.parse(data);
-    console.log(data);
-    console.log(data["tasks"].items[0].results["documents"]["classes"].category);
-    return data["tasks"].items[0].results["documents"]["classes"].category;
-    }
-    return 'error';
+        fetch(operationLocation, reqResult).then((response)=> {
+          response.json().then((json) => {
+            const result = json['tasks'].items[0].results.documents[0].class[0].category;
+            resolve(result);
+          });
+        });
+        });
+      });
+    });
   };
 
 };
