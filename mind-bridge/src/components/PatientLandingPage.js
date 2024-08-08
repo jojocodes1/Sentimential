42fde3fd3af604a6e7b5cf77391189d5735fd547
Joel Basanta <v-jobasanta@microsoft.com>
2024-08-08 11:15:25 -0700

    connected audiobooks json file to front end

--- mind-bridge/src/components/PatientLandingPage.js
+++ mind-bridge/src/components/PatientLandingPage.js
@@ -4,10 +4,21 @@ import '../App.css';
 import { FaSpotify, FaMusic, FaMicrophone, FaBook, FaPodcast } from 'react-icons/fa';
 import topTracks from '../top_tracks.json';
 import topArtistsData from '../top_artists.json';
+import audiobooks from '../user_audiobooks.json';
 
 const PatientLandingPage = () => {
- 
- 
+  const [topAudiobook, setTopAudiobook] = useState(null);
+
+  useEffect(() => {
+    // Assuming we want to display the first audiobook as the top one
+    if (audiobooks.length > 0) {
+
+      setTopAudiobook(audiobooks[0]);
+      setTopAudiobook(audiobooks[1]);
+      setTopAudiobook(audiobooks[2]);
+    }
+  }, []);
+
   const handleButtonClick = () => {
     // Open in a new window
     window.open('http://localhost:8888', '_blank', 'noopener,noreferrer');
@@ -43,7 +54,11 @@ const PatientLandingPage = () => {
  
           <div className="data-card">
             <h2><FaBook /> Audiobooks</h2>
-           
+            <ul>
+              {audiobooks.map((book, index) => (
+                <li key={index}>{book.name}</li>
+              ))}
+            </ul>
           </div>
  
           <div className="data-card">
